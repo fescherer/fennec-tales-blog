eb8d640f29b8b6f964e6c7904939fb0112055982
Felipe Scherer <felipescherer25@gmail.com>
2024-05-03 22:58:56 -0300

    :sparkles: feat: tags

--- src/features/post-list/filter-tags/index.tsx
+++ src/features/post-list/filter-tags/index.tsx
@@ -11,24 +11,26 @@ export function FilterTags({ type, alltags, tag }: FilterTagsProps) {
   const categories = getCategoriesName()
 
   return (
-    <div className="sticky top-[66px] z-50 flex flex-wrap gap-1 bg-primary">
-      <Link href="/" className={`my-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${type === '' && !tag ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>All</Link>
+    <div className="sticky top-[66px] z-50 touch-pan-y overflow-scroll whitespace-nowrap bg-primary py-4">
+      <div className="whitespace-nowrap will-change-transform">
+        <Link href="/" className={`my-2 mr-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${type === '' && !tag ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>All</Link>
 
-      {
-        categories.map(category => (
-          <Link href={`/${category}`} key={category} className={`my-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${type === category ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>
-            {category}
-          </Link>
-        ))
-      }
+        {
+          categories.map(category => (
+            <Link href={`/${category}`} key={category} className={`my-2 mr-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${type === category ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>
+              {category}
+            </Link>
+          ))
+        }
 
-      {
-        alltags.map(categoryTag => (
-          <Link href={`/tag/${categoryTag}`} key={categoryTag} className={`my-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${tag === categoryTag ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>
-            {categoryTag}
-          </Link>
-        ))
-      }
+        {
+          alltags.map(categoryTag => (
+            <Link href={`/tag/${categoryTag}`} key={categoryTag} className={`my-2 mr-2 min-w-12 rounded px-2 py-1 text-center text-sm capitalize  ${tag === categoryTag ? 'bg-secondary-label-primary text-secondary' : 'bg-secondary text-secondary-label-secondary'}`}>
+              {categoryTag}
+            </Link>
+          ))
+        }
+      </div>
     </div>
   )
 }
