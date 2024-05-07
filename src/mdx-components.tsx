dd62f3b3d162b1ceee9a40510018b594908ce59f
Felipe Scherer <felipescherer25@gmail.com>
2024-05-06 23:22:02 -0300

    :sparkles: feat: mdx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -14,65 +14,46 @@ interface IImg extends DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HT
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 id={slugyfy(children?.toString() || '')} className="inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h1 id={slugyfy(children?.toString() || '')} className="inline-block scroll-m-24 text-4xl font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h1>
     ),
     h2: ({ children }) => (
-      <h2 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h2 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-3xl font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h2>
     ),
     h3: ({ children }) => (
-      <h3 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h3 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-2xl font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h3>
     ),
     h4: ({ children }) => (
-      <h4 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h4 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-xl font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h4>
     ),
     h5: ({ children }) => (
-      <h5 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h5 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-lg font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h5>
     ),
     h6: ({ children }) => (
-      <h6 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 text-primary-label-primary">
-        <a href={`#${slugyfy(children?.toString() || '')}`}>
-          #
-          {children}
-        </a>
+      <h6 id={slugyfy(children?.toString() || '')} className="mb-2 mt-5 inline-block scroll-m-24 font-medium text-primary-label-primary">
+        <a href={`#${slugyfy(children?.toString() || '')}`}>{children}</a>
       </h6>
     ),
     table: ({ children, ...props }: any) => (
-      <table className="border-collapse" {...props}>
-        {children}
-      </table>
+      <table className="border-collapse" {...props}>{children}</table>
     ),
     p: ({ children, ...props }: any) => (
-      <p className="my-3 leading-normal tracking-wide" {...props}>
-        {children}
-      </p>
+      <p className="my-3 leading-normal tracking-wider text-primary-label-secondary" {...props}>{children}</p>
+    ),
+    hr: ({ children, ...props }: any) => (
+      <hr className="text-primary-label-tertiary" {...props}>{children}</hr>
     ),
     th: ({ children, ...props }: any) => (
-      <th className="border border-primary bg-primary text-center" {...props}>
+      <th className="border border-secondary bg-secondary text-center" {...props}>
         {children}
       </th>
     ),
@@ -82,7 +63,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       </tr>
     ),
     td: ({ children, ...props }: any) => (
-      <td className="border border-primary px-6 py-2 text-center" {...props}>
+      <td className="border border-secondary px-6 py-2 text-center" {...props}>
         {children}
       </td>
     ),
@@ -104,7 +85,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       </ol>
     ),
     li: ({ children, ...props }: any) => (
-      <li className="" {...props}>
+      <li className="ml-4" {...props}>
         {children}
       </li>
     ),
