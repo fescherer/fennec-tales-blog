7458181dbf25e07242c1c661d8fb6f5cd12c3da8
Felipe Scherer <felipescherer25@gmail.com>
2024-05-16 16:52:22 -0300

    :sparkles: add empty list state

--- src/features/post-list/index.tsx
+++ src/features/post-list/index.tsx
@@ -8,6 +8,7 @@ import { getAllTags } from '@/utils/functions'
 import { Pagination } from '@/features/pagination'
 import { PAGES_NUMBER } from '@/utils/consts'
 import { Suspense } from 'react'
+import { IconNotFound } from '@/icons/not-found'
 
 type PostListProps = {
   posts: IPost[]
@@ -32,15 +33,34 @@ export async function PostList({ type, posts, tag, query, page }: PostListProps)
       <FilterTags alltags={alltags} type={type} tag={tag} />
       {JSONLD<WebSite>(websiteJSONLD)}
 
-      <div className="m-auto grid max-w-[3000px] grid-cols-[repeat(auto-fill,_minmax(315px,_1fr))] gap-4">
-        {
-          postsByPage.map(post => (
-            <PostCard key={post.slug} post={post} />
-          ))
-        }
-      </div>
+      {!filteredPosts.length
+        ? (
+          <>
+            <div className="flex justify-center text-accent">
+              <IconNotFound />
+            </div>
 
-      <Suspense>
+            <h2 className="text-center text-3xl font-medium text-primary-label-primary">Sorry, no articles were found</h2>
+            <h2 className="text-center text-2xl text-primary-label-secondary">But you can take a look at our recent articles</h2>
+
+            <div className="m-auto mt-10 grid max-w-[1200px] grid-cols-[repeat(auto-fill,_minmax(315px,_1fr))] gap-4">
+              <PostCard key={posts[0].slug} post={posts[0]} />
+              <PostCard key={posts[1].slug} post={posts[1]} />
+              <PostCard key={posts[2].slug} post={posts[2]} />
+            </div>
+          </>
+          )
+        : (
+          <div className="m-auto grid max-w-[3000px] grid-cols-[repeat(auto-fill,_minmax(315px,_1fr))] gap-4">
+            {
+        postsByPage.map(post => (
+          <PostCard key={post.slug} post={post} />
+        ))
+      }
+          </div>
+          )}
+
+      <Suspense key="pagination">
         <Pagination posts={filteredPosts} />
       </Suspense>
     </div>
